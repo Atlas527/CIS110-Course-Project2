dfb80ac5f00542274c8dc4083f44cc1d2cb98d03
username <useremail>
2024-11-11 17:49:05 -0800

    Story Start

--- CIS110_Course_Project.py
+++ CIS110_Course_Project.py
@@ -1,1 +1,29 @@
+# Story Start - Intro
+print(f"Hello, Wanderer. Today we have a story of EPIC proportions. A story that YOU get to be a part of.")
+print(f"It's a story about a brave adventurer that some might even call....a Hero.")
+print(f"Not just any adventurer or hero, but one of minute scale and enourmous hunger.")
+input(f"\nPress the enter key to continue...")
+print(f"Before we depart for our grand adventure, I must first ask you some questions.")
+input(f"\nPress the enter key to continue...")
+heroName = input("\nThis is a story of a miniature hero doing things of gargantuan importance. What should this hero be called?: ")
+cityName = input(f"Very well, {heroName} it is. Interesting choice, and from what city does {heroName} hail?: ")
+print(f"Hmm....Well...As fate would have it, {heroName} from {cityName} is unaware that they are about the be sent on the most epic quest in the history of...quests.")
+print("Wow...that's redudant...almost as reduntant as the ellipses that are being used for pause and dramatic effect...")
+input(f"\nPress the enter key to continue")
 
+oxName = input("\nEvery hero needs their valiant steed, right? In this case your hero's steed is a 3,000 pound Dwarven Ox. What would you like to call your Oxen steed of glory?: ")
+print("Splendid. I couldn't have chosen a better name myself. Well, maybe, but I digress. This is your story, not mine. Onward.")
+
+crowName = input("Along the way you may or may not encounter a magically imbued Crow that has the ability to speak. What should we call this crow?: ")   
+print("*sigh* We can work with that I suppose.")
+princessName = input("Last question before we get to the meat and potatoes of this grand quest. Without going into too much detail, there is a Princess in this story. What should her name be?: ")
+print(f"{princessName} certainly sounds like royalty to me. Very well, adventurer. Shall we begin? Who are we kidding, you've made it this far, of course you want to hear the story. Let's go. ")
+
+input(f"\nPress the enter key to continue. (We both know you want to)")
+
+print(f"Four score and seven...apologies, wrong story. We begin this story of {heroName} the Dwarf Paladin outside the city of {cityName}. You see, life as a dwarf wasn't always easy for {heroName}. ")
+print(f"He was always the center of short jokes in the dwarven community, seeing as he had the misfortune of being a shorter than average dwarf. Though he was vertically challenged, he had a heart of gold and would do unspeakable things for food, as you'll soon discover.") 
+print(f"Often discribed as 'Strong, sturdy,and short. {heroName} was always looking for his next pint, and someone to smite." )
+
+
+                  
